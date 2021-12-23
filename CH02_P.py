b2e2e94c2fb215fe7109038c118295a3d978b902
jx-dohwan <jx7789@naver.com>
2021-12-23 14:03:31 +0900

    크롤링211223_1

---
+++ CH02_P.py
@@ -0,0 +1,10 @@
+import requests as req
+
+res = req.get('https://finance.naver.com/marketindex/?tabSel=exchange#tab_section')
+
+html = res.text
+pos = html.find('미국 USD') # 유무파악위해 쓰인다.
+print(pos)
+
+s = html.split('<span class="value">')[1].split('</span>')[0]
+print(s)
